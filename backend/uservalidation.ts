ab0b54c3fbc821ad163ed18641eb2df03a57bb97
kaushiknatua <75721842+kaushiknatua12345@users.noreply.github.com>
2026-01-28 20:38:02 +0530

    Fix username validation logic in isValidUsername

--- backend/uservalidation.ts
+++ backend/uservalidation.ts
@@ -1,5 +1,5 @@
 export function isValidUsername(username: string, password: string)
 {
-   if(!username && password.length>788) return false;
+   if(username==1 && password==1) return false;
    return true;
 }
